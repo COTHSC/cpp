c4c12219c06134ac0b82fc8c0fbe0afce2afb09c
COTHSC <thierryscully.colomban@gmail.com>
2022-02-01 11:04:29 +0100

    just to have it at school

--- C02/ex02/FixedPoint.hpp
+++ C02/ex02/FixedPoint.hpp
@@ -6,7 +6,7 @@
 /*   By: jescully <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 15:10:39 by jescully          #+#    #+#             */
-/*   Updated: 2022/01/31 09:34:16 by jescully         ###   ########.fr       */
+/*   Updated: 2022/01/31 11:00:36 by jescully         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,38 @@
 class Fixed 
 {
 	public:
+        //constructors & destructor
 		Fixed();
 		Fixed(const int value);
 		Fixed(const float value);
 		Fixed(Fixed const & src);
+		~Fixed();
+
+        //conversions
 		int toInt( void ) const;
 		float toFloat( void ) const;
-		~Fixed();
+
+        //getters and setters
 		int		getBits( void ) const;
 		void	setBits(int const raw);
+
         bool    isInt (void) const;
+        //Hello operator (comparisons)
+        bool    operator<(Fixed const & rhs) const;
+        bool    operator>(Fixed const & rhs) const;
+        bool    operator<=(Fixed const & rhs) const;
+        bool    operator>=(Fixed const & rhs) const;
+        bool    operator==(Fixed const & rhs) const;
+        bool    operator!=(Fixed const & rhs) const;
+
+
+        //Hello operator (calculus)
+        Fixed operator+(const Fixed & rhs) const;
+        Fixed operator*(const Fixed & rhs) const;
+        Fixed operator-(const Fixed & rhs) const;
+        Fixed operator/(const Fixed & rhs) const;
+
+        //Hello operator (assignation)
 		Fixed &  operator=(const Fixed & rhs);
 	private:
 		int		_getFractionalBits( void ) const;
